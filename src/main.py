f337aeb22d916755682f48dd6d3a1d10981bcf6b
Orfeu <orfeu8@live.fr>
2023-11-12 17:36:58 +0100

    qlq optimisation, qlq tests, début utilisation igraph, commentaire explicatif  sur l'utilisation de valide_subset_of_S12

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import itertools
 from itertools import permutations
 from icecream import ic
+import igraph as ig
 
 len_tetra = 4
 
@@ -65,6 +66,8 @@ ic(len(S_114))
 S_126 = S_12 | S_114
 ic(len(S_126))
 
+
+
 #create and print all possible variations of S_6, i.e. all subsets of S_12 where we select at most 1 element from every pair of elements that are permutations of each other
 S_12_list = list(S_12)
 S_6_A = []
@@ -77,31 +80,93 @@ for i in range(12):
             break
 ic(len(S_6_A))
 ic(len(S_6_B))
-S_6_all = []
+valid_subsets_of_S12 = []
 # do the cartesian product 6 times of {0,1,2}
 # for each element, if 0 then nothing, if 1 then take it in S_6_A, if 2 then take it in S_6_B
 for p in itertools.product([0,1,2], repeat=6):
-    S_6_all.append([S_6_A[i] if p[i] == 1 else S_6_B[i] if p[i] == 2 else None for i in range(6)])
-ic(len(S_6_all))
+    valid_subsets_of_S12.append([S_6_A[i] if p[i] == 1 else S_6_B[i] if p[i] == 2 else None for i in range(6)])
+ic(len(valid_subsets_of_S12))
+
+
 
 
 ####################################################################################################
 # création de graphe à partir de sous esembles de S_126
 
-# Attention, si on utilise cette fonction pour avoir le graphe d'un language entier, elle va potentiellement créer plusieurs fois les mêms noeuds (si ils apparaissents dans plusieurs mots)
+# OPTI: à voir si la structure set est la plus opti à utiliser
+
+# Cette fonction crée les arrêtes ( exemple (AA, AG), à partir d'un tetranucléotide 
+# Si on utilise cette fonction sur tout les tetranucléotides d'un code, il faudra verifier qu'aucune arrete n'est crée plusieurs fois
 def get_nod_and_edge_tetra(tetra):
     nods = set()
     edges = set()
     for slice_idx in range(1,len_tetra):
-        nods.add(tetra[0:slice_idx])
+
+        first_slice = tetra[0:slice_idx]
+        second_slice = tetra[slice_idx:]
+
+        nods.add(first_slice)
         if slice_idx == 2:
-            if tetra[slice_idx:] != tetra[0:slice_idx]:
-                nods.add(tetra[slice_idx:])
+            #On évite de remettre un noeud qui est déjà dans le graphe (par ex pour "AAAA", on ne met pas 2 fois le noeud "AA" dans nods)
+            if second_slice != first_slice:
+                nods.add(second_slice)
         else:
-            nods.add(tetra[slice_idx:])
-        edges.add((tetra[0:slice_idx], tetra[slice_idx:])) #ici je refais le slicing que j'ai déjà fais, faudrait ne pas le faire
+            nods.add(second_slice)
+        edges.add((first_slice, second_slice)) 
     return nods, edges
 
 
+# créer le graph à partir des tetra d'un code 
+# OPTI:  à voir si l'union des set est opti à utiliser
+def get_graph_from_code(code):
+    nods = set()
+    edges = set()
+    for tetra in code:
+        nods_tetra, edges_tetra = get_nod_and_edge_tetra(tetra)
+        nods = nods | nods_tetra
+        edges = edges | edges_tetra
+    return nods, edges
+    
+
+####################################################################################################
+
+#tests
+ 
 
 
+
+####################################################################################################
+# Test de présence de cycle
+
+# S_126_list = list(S_126)
+# S_126_list = sorted(S_126_list)
+
+# nods, edges = get_graph_from_code(S_126_list[0:5])
+
+# # Example for a directed graph
+# # Create or load your graph
+# dg = ig.Graph(edges, directed=True)
+
+# # Check if the graph is a DAG (Directed Acyclic Graph)
+# if dg.is_dag():
+#     print("The directed graph is acyclic (no cycles).")
+# else:
+#     print("The directed graph has at least one cycle.")
+
+
+####################################################################################################
+
+# for longueur_de_code in range(1, 121):
+#     for valid_subset_of_S12 in valid_subsets_of_S12:
+#         for subset_of_S_114 in parties(S_114, taille=longueur_de_code):
+#             code = valid_subset_of_S12 | subset_of_S_114
+#             # ...
+        
+
+
+# for longueur_de_code in range(1, 121):
+#     for longueur_S12_i in range (0,7):
+#         for valid_subset_of_S12 in parties(S_12_i, taille=longueur_S12_i):
+#             for subset_of_S_114 in parties(S_114, taille=longueur_de_code):
+#                 code = valid_subset_of_S12 | subset_of_S_114
+                # ... 
